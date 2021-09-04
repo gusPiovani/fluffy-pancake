52aea0958ac83d8c11c289ae88150c88329acbaa
Gustavo Piovani <79936110+GHyrow@users.noreply.github.com>
2021-09-04 11:09:07 -0300

    evo
    less five years

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById('hours');
 const minsEl = document.getElementById('mins');
 const secondsEl = document.getElementById('seconds');
 
-const newYears = "2 mar 2035";
+const newYears = "2 mar 2030";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
